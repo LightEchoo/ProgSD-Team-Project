1d4f9bd23f761d5d9453b3cd0ec58050b7f3798f
2741651q <2741651Q@student.gla.ac.uk>
2023-11-01 00:39:31 +0000

    更新 rent_start 方法

--- BE_Function.py
+++ BE_Function.py
@@ -120,9 +120,9 @@ def rent_start(car_id, user_name):
     #print(user_info)
     #print(car_info)
 
-    if user_info[3] != 0:
+    '''if user_info[3] != 0:
         connect.close()
-        return "DepositError"
+        return "DepositError"'''
 
     existing_orders = SqlFunction.get_one_user_orders(user_name)
     for order in existing_orders:
